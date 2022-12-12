b5334c97293351db16c03ed02a34359c1243aa91
Jean Luc <jlucafricain@gmail.com>
2022-12-12 11:22:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - A program that prints a line with a puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		return (0);
 }
